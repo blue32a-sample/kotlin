6e401772a3e97850e9db26b78de220db2402b109
blue32a <blue32a.jp@gmail.com>
2021-08-15 17:27:31 +0900

    Classes

---
+++ src/01_introduction/05_classes/classes.kt
@@ -0,0 +1,15 @@
+class Customer
+
+class Contact(val id: Int, var email: String)
+
+fun main() {
+    val cunstomer = Customer()
+
+    val contact = Contact(1, "mary@gmail.com")
+
+    println(contact.id)
+    println(contact.email)
+
+    contact.email = "jane@gmail.com"
+    println(contact.email)
+}
