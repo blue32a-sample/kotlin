23390c87d69e5266bb112ae616ab3554153fbdac
blue32a <blue32a.jp@gmail.com>
2021-12-31 14:34:50 +0900

    Delegated Properties

---
+++ src/07_delegation/02_delegated-properties/storing-properties-in-a-map.kt
@@ -0,0 +1,12 @@
+class User(val map: Map<String, Any?>) {
+    val name: String by map
+    val age: Int by map
+}
+
+fun main() {
+    val user = User(mapOf(
+        "name" to "John Doe",
+        "age" to 25
+    ))
+    println("name = ${user.name}, age = ${user.age}")
+}
